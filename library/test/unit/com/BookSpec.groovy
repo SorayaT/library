8652068ba941deb9c969931d139a331f9203613f
SorayaT <b3042928@my.shu.ac.uk>
2015-03-16 11:57:59 +0000

    Tested the Book domain class.

--- library/test/unit/com/BookSpec.groovy
+++ library/test/unit/com/BookSpec.groovy
@@ -15,6 +15,16 @@ class BookSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test toString"() {
+	when:"A Book has a title, author and isbn"
+	def book=new Book(title:'1984', 
+		author:'George Orwell',
+		isbn:'9780141182957',
+		dateBorrowed: new Date('22/02/2015'),
+		returnDate: new Date('19/03/2015'),
+		student: 'Student Borrowingthisbook'
+	then:"the to String methos will merge them."
+	book.toString()=='1984, George Orwell, 9780141182957, 22/02/2015, 	19/03/2015, Student Borrowingthisbook'
+)
     }
 }
